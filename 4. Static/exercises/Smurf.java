d815b2f1d591dff04975c6c696fb248286f29631
SamuelRussell <samuel.prajasantosa@gmail.com>
2022-03-12 14:05:48 +0800

    Smurf Java Exercise (Not completed)

--- 4. Static/exercises/Smurf.java
+++ 4. Static/exercises/Smurf.java
@@ -2,15 +2,25 @@ package exercises;
 
 public class Smurf {
 
+	private int number;
 	private String name;
+	private static int instance = 0;
 
 	public static Smurf createSmurf(String name) {
-		System.out.println("Creating " + name + " Smurf");
-		return new Smurf(name);
+		if (instance > 1) {
+			System.out.println("This smurf had been created previously");
+		}
+		else {
+			System.out.println("Creating " + name + " Smurf");
+			return new Smurf(name);
+		}
+		return null;
 	}
 
 	private Smurf(String name) {
 		this.name = name;
+		++instance;
+		this.number = instance;
 	}
 
 	public void printName() {
@@ -21,4 +31,19 @@ public class Smurf {
 		System.out.println(name + " Smurf is eating Smurfberries.");
 	}
 
+	public int getNumber() {
+		return number;
+	}
+
+	public static void main(String[] args){
+		Smurf.createSmurf("Papa");
+		Smurf.createSmurf("Mama");
+		Smurf.createSmurf("Smurfette");
+		Smurf.createSmurf("Papa");
+		Smurf.createSmurf("Papa");
+		Smurf.createSmurf("Papa");
+		Smurf.createSmurf("Smurfette");
+	}
+
 }
+
